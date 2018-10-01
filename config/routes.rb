ab600e3564a0b857a3b9a5f969cfa77d4917c714
Cedric David <cedric.david@btopenworld.com>
2018-10-01 10:20:45 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show]
+  resources :posts, only: [:index, :show, :new]
   
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,30 +1,28 @@
+
+
 require 'rails_helper'
 
-describe 'navigate' do
-  before do
-    @post = Post.create(title: "My Post", description: "My post desc")
-  end
-
-  it 'shows the title on the show page in a h1 tag' do
-    visit "/posts/#{@post.id}"
-    expect(page).to have_css("h1", text: "My Post")
-  end
-
-  it 'to post pages' do
-    visit "/posts/#{@post.id}"
+describe 'new post' do
+  it 'ensures that the form route works with the /new action' do
+    visit new_post_path
     expect(page.status_code).to eq(200)
   end
 
-  it 'shows the description on the show page in a p tag' do
-    visit "/posts/#{@post.id}"
-    expect(page).to have_css("p", text: "My post desc")
-  end
+  it 'renders HTML in the /new template' do
+   visit new_post_path
+   expect(page).to have_content('Post Form')
+ end
+
+ it "displays a new post form that redirects to the index page, which then contains the submitted post's title and description" do
+  visit new_post_path
+  fill_in 'post_title', with: 'My post title'
+  fill_in 'post_description', with: 'My post description'
+
+  click_on 'Submit Post'
+
+  expect(page.current_path).to eq(posts_path)
+  expect(page).to have_content('My post title')
+  expect(page).to have_content('My post description')
 end
 
-describe 'index page' do
-  it 'links to post page' do
-    second_post = Post.create(title: "My Title", description: "My post description")
-    visit posts_path
-    expect(page).to have_link(second_post.title, href: post_path(second_post))
-  end
 end
